10561f85fc70b63c3f476d7e2761c9a179340fd2
Grandpere <lorenzo.marozzo@gmail.com>
2020-07-18 13:11:03 +0200

    update subscriber with constructor typehint & customQuery to sorting conferences by year and city ASC

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -2,15 +2,17 @@
 
 namespace App\EventSubscriber;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Twig\Environment;
 
 class TwigEventSubscriber implements EventSubscriberInterface
 {
     private $twig;
     private $conferenceRepository;
 
-    public function __construct($twig, $conferenceRepository)
+    public function __construct(Environment $twig, ConferenceRepository $conferenceRepository)
     {
         $this->twig = $twig;
         $this->conferenceRepository = $conferenceRepository;
