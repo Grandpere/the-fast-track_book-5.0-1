0bfc1b6cc1b2805f9100849e03d29d12aa94fef8
Grandpere <lorenzo.marozzo@gmail.com>
2020-07-18 15:26:37 +0200

    ignore uploaded files & update easyAdmin config with column image

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,6 +9,7 @@ use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -36,6 +37,7 @@ class ConferenceController extends AbstractController
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()) {
             $comment->setConference($conference);
+            /** @var UploadedFile $photo */
             if($photo = $form['photo']->getData()) {
                 $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
                 try {
