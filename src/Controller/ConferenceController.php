f305441f2509b53e43262f8ad20b184f1165707c
Grandpere <lorenzo.marozzo@gmail.com>
2020-09-26 11:31:00 +0200

    update controller from twig dependency injection in method to dependency injection to constructor

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,22 +8,34 @@ use App\Form\CommentFormType;
 use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
+    private $twig;
+    private $entityManager;
+    private $bus;
+
+    public function __construct(Environment $twig, EntityManagerInterface $entityManager, MessageBusInterface $bus)
+    {
+        $this->twig = $twig;
+        $this->entityManager = $entityManager;
+        $this->bus = $bus;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
     public function index(ConferenceRepository $conferenceRepository)
     {
-        $response =  new Response($this->render('conference/index.html.twig', [
+        $response = new Response($this->twig->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
         ]));
         $response->setSharedMaxAge(3600);
@@ -36,7 +48,7 @@ class ConferenceController extends AbstractController
      */
     public function conferenceHeader(ConferenceRepository $conferenceRepository)
     {
-        $response =  new Response($this->render('conference/header.html.twig', [
+        $response = new Response($this->twig->render('conference/header.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
         ]));
         $response->setSharedMaxAge(3600);
@@ -47,27 +59,25 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conference/{slug}", name="conference")
      */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir, MessageBusInterface $bus)
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir)
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $comment->setConference($conference);
-            /** @var UploadedFile $photo */
-            if($photo = $form['photo']->getData()) {
+            if ($photo = $form['photo']->getData()) {
                 $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
                 try {
                     $photo->move($photoDir, $filename);
                 } catch (FileException $e) {
-                    // Unable to upload the photo, give up
+                    // unable to upload the photo, give up
                 }
                 $comment->setPhotoFilename($filename);
             }
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($comment);
-            $entityManager->flush();
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
 
             $context = [
                 'user_ip' => $request->getClientIp(),
@@ -76,7 +86,7 @@ class ConferenceController extends AbstractController
                 'permalink' => $request->getUri(),
             ];
 
-            $bus->dispatch(new CommentMessage($comment->getId(), $context));
+            $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
         }
@@ -84,7 +94,7 @@ class ConferenceController extends AbstractController
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
-        return new Response($this->render('conference/show.html.twig', [
+        return new Response($this->twig->render('conference/show.html.twig', [
             'conference' => $conference,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
